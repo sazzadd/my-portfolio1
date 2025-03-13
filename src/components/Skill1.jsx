71834c8310fe566f33ba3e893081630716adf581
sazzad <sazzadador@gmail.com>
2025-03-13 22:56:57 +0600

    fix align

---
+++ src/components/Skill1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skill1 = () => {
+    return (
+        <div className='bg-[#0f1629] text-white py-8 flex items-center justify-center'>
+            
+        </div>
+    );
+};
+
+export default Skill1;
